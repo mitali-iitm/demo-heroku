a16aca5b5defc19f6715e0e650e2a8581a4901fc
muugumuugu <mogimogi120501@gmail.com>
2022-07-19 23:41:36 +0530

    code

--- prj.py
+++ prj.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.title('GA 8')
-#st.subheader('Dividing App')
-#placeholder_diviend = st.empty()
-#placeholder_divisor = st.empty()
-#n = placeholder_dividend.number_input('Enter Dividend')
-#d= placeholder_divisor.number_input('Enter Divisor ')
+st.subheader('Dividing App')
+placeholder_diviend = st.empty()
+placeholder_divisor = st.empty()
+n = placeholder_dividend.number_input('Enter Dividend')
+d= placeholder_divisor.number_input('Enter Divisor ')
+st.write(n/d)
