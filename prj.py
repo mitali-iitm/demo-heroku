ea2e8d9074c3c3eec23ab67fb5f87986f826ece6
muugumuugu <mogimogi120501@gmail.com>
2022-07-19 23:49:47 +0530

    code

--- prj.py
+++ prj.py
@@ -5,4 +5,5 @@ placeholder_dividend = st.empty()
 placeholder_divisor = st.empty()
 n = placeholder_dividend.number_input('Enter Dividend')
 d= placeholder_divisor.number_input('Enter Divisor ')
-st.write(n/d)
+if d!=0:
+	st.write(n/d)
