0d1a14351bd5434291c31a0da99b9bfe0dc0daf4
muugumuugu <mogimogi120501@gmail.com>
2022-07-19 23:38:25 +0530

    code

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"21f3001406@student.onlinedegree.iitm.ac.in\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
